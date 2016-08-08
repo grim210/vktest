1170b50cea58fd2e8ed4d8988c5488fb012cfbea
Joe Brown <brownj3199@live.com>
2016-08-07 21:40:56 -0500

    Fixed build on Windows.

--- global.h
+++ global.h
@@ -7,7 +7,11 @@
 #include <sstream>
 #include <string>
 
-#define VK_USE_PLATFORM_XCB_KHR
+#if defined(__linux__)
+  #define VK_USE_PLATFORM_XCB_KHR
+#elif defined(_WIN32)
+  #define VK_USE_PLATFORM_WIN32_KHR
+#endif
 #include <vulkan/vulkan.h>
 
 #include <glm/glm.hpp>
